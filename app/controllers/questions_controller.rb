bbd2d7ec00aebef3933a10ba48e9882cb215e1a7
Dodie324 <drmoore324@gmail.com>
2015-03-24 17:31:34 -0400

    User authentication added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @questions = Question.all
   end
@@ -14,15 +16,35 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
-    @question.created_by = current_user.id
-    binding.pry
+    @question.user = current_user
     if @question.save
-      binding.pry
       flash[:notice] = 'Question added!'
-      redirect_to '/'
+      redirect_to @question
     else
+      flash[:notice] = 'Question not added!'
       render :new
     end
+  end
+
+  def edit
+    id = params[:id]
+    @question = Question.find(id)
+  end
+
+  def update
+    binding.pry
+    id = params[:id]
+    @question = Question.find(id)
+    @question.update!(question_params)
+    redirect_to @question
+  end
+
+  def destroy
+    id = params[:id]
+    @question = Question.find(id)
+    @question.destroy
+    flash[:notice] = 'Question deleted.'
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'questions#index'
+  devise_for :users
   resources :questions
 
 
